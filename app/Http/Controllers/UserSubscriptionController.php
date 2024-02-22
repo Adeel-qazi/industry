ac430afbb4efbc19aa9059f447fdec6380f50b78
adeel-raza <adeelraza@hnhtechsolutions.com>
2024-02-22 14:06:07 +0500

    subscribe the package

--- app/Http/Controllers/UserSubscriptionController.php
+++ app/Http/Controllers/UserSubscriptionController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\SubscribedPackageRequest;
 use App\Models\Subscription;
 use App\Models\UserSubscription;
 use Illuminate\Http\Request;
+
 // use Illuminate\Support\Facades\Request;
 
 class UserSubscriptionController extends Controller
@@ -13,66 +14,82 @@ class UserSubscriptionController extends Controller
     public function index()
     {
 
-       if (auth()->check()) {
-        $user = auth()->user();
+        if (auth()->check()) {
+            $user = auth()->user();
 
-        if ($user->role == 'user') {
+            if ($user->role == 'user') {
 
-            $subscriptions = Subscription::orderBy('id','DESC')->get();
+                $subscriptions = Subscription::orderBy('id', 'DESC')->get();
 
-            return response()->json([
-                'status' => true,
-                'message' => 'Successfully fetch All packages',
-                'data' => $subscriptions,
-            ], 200);
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Successfully fetch All packages',
+                    'data' => $subscriptions,
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'You do not have the permissions to fetch All packages.',
+                ], 403);
+            }
         } else {
             return response()->json([
                 'status' => false,
-                'message' => 'You do not have the permissions to fetch All packages.',
-            ], 403);
+                'message' => 'Authentication required to fetch All packages.',
+            ], 401);
         }
-    } else {
-        return response()->json([
-            'status' => false,
-            'message' => 'Authentication required to fetch All packages.',
-        ], 401);
-    }
     }
 
 
-    public function storePackage(Request $request, Subscription $subscription)
+    public function storePackage(SubscribedPackageRequest $request)
     {
 
-    try {
+        try {
+            $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
 
-        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
-
-      $res = $stripe->tokens->create([
-            'card' => [
-              'number' => $request->number,
-              'exp_month' => $request->exp_month,
-              'exp_year' => $request->exp_year,
-              'cvc' => $request->cvc,
-            ],
-          ]);
-
-        $response =  $stripe->charges->create([
-            'package' => $request->packageId,
-            // 'amount' => round($request->amount, 2) * 100,
-            'currency' => "USD",
-            'source' => $request->token,
+            $response = $stripe->charges->create([
+                'amount' => $request->amount * 100,
+                'currency' => 'usd',
+                'source' => $request->source,
+                'description' => 'Testing Charge',
             ]);
 
-            return response()->json([$response->status],201);
 
-    } catch (\Throwable $th) {
-                return response()->json(['status' => false, 'error' => $th->getMessage()]);
-    }
+            if (auth()->check()) {
+                $user = auth()->user();
+
+                if ($user->role == 'user') {
+                    UserSubscription::create(['user_id' => $user->id, 'package_id' => $request->packageId, 'payment_id' => $response->source]);
+
+                    return response()->json([
+                        'status' => true,
+                        'message' => "Successfully package has been subscribed ",
+                    ], 200);
+                } else {
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'You do not have the permissions to subscribe the package.',
+                    ], 403);
+                }
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Authentication required to subscribe the package.',
+                ], 401);
+            }
+
+
+
+        } catch (\Throwable $th) {
+            return response()->json(['status' => false, 'error' => $th->getMessage()], 500);
+        }
+
+
     }
 
     public function cancelPackage()
     {
-        return response()->json(['status' => true,'message'=>'Package has been cancelled']);
+        return response()->json(['status' => true, 'message' => 'Package has been cancelled']);
 
     }
 }
