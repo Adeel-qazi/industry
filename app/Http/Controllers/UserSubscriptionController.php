3e46bd7082616ccbf3fbd0887024d16c52d3dbd1
adeel-raza <adeelraza@hnhtechsolutions.com>
2024-02-22 17:42:01 +0500

    subscrive

--- app/Http/Controllers/UserSubscriptionController.php
+++ app/Http/Controllers/UserSubscriptionController.php
@@ -59,7 +59,7 @@ class UserSubscriptionController extends Controller
                 $user = auth()->user();
                 if ($user->role == 'user') {
                     $user->update(['status'=>'paid']);
-                    UserSubscription::create(['user_id' => $user->id, 'subscription_id ' => $request->packageId, 'payment_id' => $response->id]);
+                    UserSubscription::create(['user_id' => $user->id, 'subscription_id ' => $request->subscription_id, 'payment_id' => $response->id]);
 
                     return response()->json([
                         'status' => true,
